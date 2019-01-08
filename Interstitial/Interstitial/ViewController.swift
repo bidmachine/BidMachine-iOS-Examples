db6b79ea54df70ce5ebc7f6469fc131a9785bcf3
Yaroslav Skachkov <y.skachkov@appodeal.com>
2019-01-08 12:26:15 +0300

    Projects with gitignores were added.

---
+++ Interstitial/Interstitial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Interstitial
+//
+//  Created by Yaroslav Skachkov on 1/8/19.
+//  Copyright © 2019 Appodeal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
