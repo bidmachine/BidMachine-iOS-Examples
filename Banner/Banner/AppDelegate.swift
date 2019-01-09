77bb30ddad0cf77a8f5eede27a5c32af81d60053
Yaroslav Skachkov <y.skachkov@appodeal.com>
2019-01-09 14:36:48 +0300

    Banner project updated.

--- Banner/Banner/AppDelegate.swift
+++ Banner/Banner/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import BidMachine
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let configuration = BDMSdkConfiguration()
+        configuration.testMode = true
+        BDMSdk.shared().startSession(withSellerID: "1", configuration: configuration) {
+            print("BidMachine SDK has initialized")
+        }
         return true
     }
 
--- Banner/Banner/BannerVC.swift
+++ Banner/Banner/BannerVC.swift
@@ -7,14 +7,65 @@
 //
 
 import UIKit
+import BidMachine
 
 class BannerVC: UIViewController {
-
+    
+    @IBOutlet weak var removeBannerButton: UIButton!
+    
+    private lazy var bannerView: BDMBannerView = {
+        let bannerView = BDMBannerView()
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        return bannerView
+    }()
+    
+    private lazy var request: BDMRequest = {
+        return BDMRequest()
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        removeBannerButton.isEnabled = false
+        bannerView.delegate = self
+        bannerView.rootViewController = self
     }
 
-
+    @IBAction func loadBanner(_ sender: UIButton) {
+        bannerView.adSize = .size320x50
+        bannerView.make(request)
+    }
+    
+    @IBAction func removeBanner(_ sender: UIButton) {
+        bannerView.removeFromSuperview()
+        removeBannerButton.isEnabled = false
+    }
+    
+    private func configureBannerView() {
+        let bannerViewWidthConstraint = bannerView.widthAnchor.constraint(equalToConstant: CGSizeFromBDMSize(bannerView.adSize).width)
+        let bannerViewHeightConstraint = bannerView.heightAnchor.constraint(equalToConstant: CGSizeFromBDMSize(bannerView.adSize).height)
+        let bannerViewHorizontalCenter = bannerView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+        let bannerViewVerticalCenter = bannerView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -120)
+        NSLayoutConstraint.activate([bannerViewWidthConstraint, bannerViewHeightConstraint, bannerViewVerticalCenter, bannerViewHorizontalCenter])
+    }
 }
 
+extension BannerVC: BDMBannerDelegate {
+    func bannerViewDidExpire(_ bannerView: BDMBannerView) {
+        print("Banner view expired")
+    }
+    
+    func bannerViewRecieveUserInteraction(_ bannerView: BDMBannerView) {
+        print("Banner view received user interaction")
+    }
+    
+    func bannerView(_ bannerView: BDMBannerView, readyToPresentAd auctionInfo: BDMAuctionInfo) {
+        print("Banner view is ready to present ad")
+        removeBannerButton.isEnabled = true
+        self.view.addSubview(self.bannerView)
+        configureBannerView()
+    }
+    
+    func bannerView(_ bannerView: BDMBannerView, failedWithError error: Error) {
+        print("Banner view failed on loading with error: \(error)")
+    }
+}
