637f03e7b07569104be77f7df9aab04eecdd4945
Yaroslav Skachkov <y.skachkov@appodeal.com>
2019-01-09 14:37:11 +0300

    Interstitial project updated.

--- Interstitial/Interstitial/AppDelegate.swift
+++ Interstitial/Interstitial/AppDelegate.swift
@@ -16,7 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let configuration = BDMSdkConfiguration()
+        configuration.testMode = true
+        BDMSdk.shared().startSession(withSellerID: "1", configuration: configuration) {
+            print("BidMachine SDK has initialized")
+        }
         return true
     }
 
--- Interstitial/Interstitial/InterstitialVC.swift
+++ Interstitial/Interstitial/InterstitialVC.swift
@@ -7,14 +7,80 @@
 //
 
 import UIKit
+import BidMachine
 
 class InterstitialVC: UIViewController {
 
+    @IBOutlet weak var presentButton: UIButton!
+    
+    @IBOutlet weak var interstitialTypeSegmentedControl: UISegmentedControl!
+    
+    private lazy var interstitial: BDMInterstitial = {
+        return BDMInterstitial()
+    }()
+    
+    private lazy var request: BDMRequest = {
+        return BDMRequest()
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        presentButton.isEnabled = false
+        interstitial.delegate = self
     }
 
+    @IBAction func loadNonVideoAd(_ sender: UIButton) {
+        interstitial.type = selectInterstitialType()
+        interstitial.make(request)
+    }
+    
+    @IBAction func presentInterstitial(_ sender: UIButton) {
+        interstitial.present(fromRootViewController: self)
+    }
+    
+    private func selectInterstitialType() -> BDMFullscreenAdType {
+        var type: BDMFullscreenAdType
+        switch interstitialTypeSegmentedControl.titleForSegment(at: interstitialTypeSegmentedControl.selectedSegmentIndex) {
+        case "Video" : type = .fullscreenAdTypeVideo
+        case "NonVideo" : type = .fullsreenAdTypeBanner
+        default : type = .fullscreenAdTypeAll
+        }
+        return type
+    }
+}
 
+extension InterstitialVC: BDMInterstitialDelegate {
+    func interstitialWillPresent(_ interstitial: BDMInterstitial) {
+        print("Interstitial will present ad")
+    }
+    
+    func interstitialDidExpire(_ interstitial: BDMInterstitial) {
+        print("Interstitial expired")
+    }
+    
+    func interstitialDidDismiss(_ interstitial: BDMInterstitial) {
+        print("Interstitial dismissed")
+        presentButton.isEnabled = false
+    }
+    
+    func interstitialRecieveUserInteraction(_ interstitial: BDMInterstitial) {
+        print("Interstitial received user interaction")
+    }
+    
+    func interstitial(_ interstitial: BDMInterstitial, readyToPresentAd auctionInfo: BDMAuctionInfo) {
+        print("Interstitial is ready to present ad")
+        presentButton.isEnabled = true
+    }
+    
+    func interstitial(_ interstitial: BDMInterstitial, failedWithError error: Error) {
+        print("Interstitial failed on loading with error: \(error)")
+        presentButton.isEnabled = false
+    }
+    
+    func interstitial(_ interstitial: BDMInterstitial, failedToPresentWithError error: Error) {
+        print("Interstitial failed to present ad with error: \(error)")
+        presentButton.isEnabled = false
+    }
+    
 }
 
